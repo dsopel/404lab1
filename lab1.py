3dcc3ebd74f35007a79ffb601d996d9ee782ee08
dsopel <dsopel@ualberta.ca>
2016-09-07 15:18:09 -0600

    modified to print source code

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.post('http://ccid-eddieantonio.rhcloud.com/dsopel')
-print(response.status_code)
+response = requests.get('https://raw.githubusercontent.com/dsopel/404lab1/master/lab1.py')
+print(response.text)
